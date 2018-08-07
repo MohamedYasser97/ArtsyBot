10c3e6176d62e9bfbe6facb6b86b0cd1e3b3bfd4
Mohamed Yasser <30673997+MohamedYasser97@users.noreply.github.com>
2018-08-07 19:02:27 +0200

    added comment

--- starter.js
+++ starter.js
@@ -1,3 +1,9 @@
+/*
+	This file is responsible for restarting the main bot file whenever it exits.
+	The bot file can only exit when there is no internet connection or when
+	an image upload or tweet sending fails.
+*/
+
 while(1){
 	require('child_process').execSync('node bot.js',{stdio:[0,1,2]});
 }
