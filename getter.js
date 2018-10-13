45eecaab48e8dd390397ac643fd0c1ab9e8bf03a
Mohamed Yasser <30673997+MohamedYasser97@users.noreply.github.com>
2018-10-13 13:55:25 +0200

    added URL checking case

--- getter.js
+++ getter.js
@@ -37,7 +37,7 @@ const getURL =  function(subreddit){
 
             }
 
-            if (!image) 
+            if (!image || image=='-') 
             	return resolve('-1');
 
             if(crossParent)
